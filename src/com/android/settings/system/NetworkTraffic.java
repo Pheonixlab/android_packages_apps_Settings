AOSPA-sweet/android_packages_apps_ParanoidLab

ParanoidLab: Notch friendly network traffic [2/2]

src/com/aospa/lab/fragments/NetworkTraffic.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aospa.lab.fragments;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.aospa.support.preferences.SystemSettingListPreference;
+import com.aospa.support.preferences.SystemSettingSeekBarPreference;
+
+public class NetworkTraffic extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private SystemSettingListPreference mLocation;
+    private SystemSettingSeekBarPreference mThreshold;
+    private SystemSettingListPreference mIndicatorMode;
+    private SystemSettingSeekBarPreference mInterval;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        addPreferencesFromResource(R.xml.network_traffic);
+
+        mLocation = (SystemSettingListPreference) findPreference("network_traffic_location");
+        mIndicatorMode = (SystemSettingListPreference) findPreference("network_traffic_mode");
+        mThreshold = (SystemSettingSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+        mInterval = (SystemSettingSeekBarPreference) findPreference("network_traffic_refresh_interval");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AOSPA;
+    }
+}

src/com/aospa/lab/fragments/StatusBarSettings.java
@@ -2,7 +2,10 @@ package com.aospa.lab.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -34,22 +37,38 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 
-public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+import com.aospa.support.preferences.SystemSettingMasterSwitchPreference;
+
+public class StatusBarSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+private static final String KEY_NETWORK_TRAFFIC = "network_traffic_state";
+
+private SystemSettingMasterSwitchPreference mNetworkTraffic;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.statusbar);
-
         PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mNetworkTraffic = (SystemSettingMasterSwitchPreference)
+                findPreference(KEY_NETWORK_TRAFFIC);
+        mNetworkTraffic.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NETWORK_TRAFFIC_STATE, 0) == 1));
+        mNetworkTraffic.setOnPreferenceChangeListener(this);
 
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    	if (preference == mNetworkTraffic) {
+            boolean enabled = (boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_STATE, enabled ? 1 : 0);
+            return true;
+	}
         return false;
     }
 

Pheonixlab/android_packages_apps_Settings

src/com/android/settings/system/NetworkTraffic.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.system;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.p404.support.preferences.SystemSettingListPreference;
+import com.p404.support.preferences.SystemSettingSeekBarPreference;
+
+public class NetworkTraffic extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private SystemSettingListPreference mLocation;
+    private SystemSettingSeekBarPreference mThreshold;
+    private SystemSettingListPreference mIndicatorMode;
+    private SystemSettingSeekBarPreference mInterval;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        addPreferencesFromResource(R.xml.network_traffic);
+
+        mLocation = (SystemSettingListPreference) findPreference("network_traffic_location");
+        mIndicatorMode = (SystemSettingListPreference) findPreference("network_traffic_mode");
+        mThreshold = (SystemSettingSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+        mInterval = (SystemSettingSeekBarPreference) findPreference("network_traffic_refresh_interval");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.P404;
+    }
+}

src/com/android/settings/system/StatusBarSettings.java
@@ -2,7 +2,10 @@ package com.android.settings.system;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -34,22 +37,38 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 
-public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+import com.p404.support.preferences.SystemSettingMasterSwitchPreference;
+
+public class StatusBarSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+private static final String KEY_NETWORK_TRAFFIC = "network_traffic_state";
+
+private SystemSettingMasterSwitchPreference mNetworkTraffic;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.statusbar);
-
         PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mNetworkTraffic = (SystemSettingMasterSwitchPreference)
+                findPreference(KEY_NETWORK_TRAFFIC);
+        mNetworkTraffic.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NETWORK_TRAFFIC_STATE, 0) == 1));
+        mNetworkTraffic.setOnPreferenceChangeListener(this);
 
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    	if (preference == mNetworkTraffic) {
+            boolean enabled = (boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_STATE, enabled ? 1 : 0);
+            return true;
+	}
         return false;
     }
 